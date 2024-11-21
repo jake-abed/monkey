0636e81fcfbfaf280543bea13566068c5ac543d0
Jake Abed <jakeabed@proton.me>
2024-11-21 14:13:58 -0800

    Update gitignore and begin AST

---
+++ ast/ast.go
@@ -0,0 +1,50 @@
+package ast
+
+import "github.com/jake-abed/monkey/token"
+
+// The Node interface can also be either Statement or Expression
+// interfaces as well. The methods on those are just dummy methods.
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// The root of every single AST our parser produces!
+
+type Program struct {
+	Statements []Statement
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token in this case.
+	Value string
+}
+
+func (i *Identifier) expressionNode()
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
