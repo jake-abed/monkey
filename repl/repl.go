dec31e4f631bb064dcf0c98ebca0dbbc83f827de
Jake Abed <jakeabed@proton.me>
2024-12-02 12:26:18 -0800

    Update repl to use evaluator (ints only)

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/jake-abed/monkey/lexer"
 	"github.com/jake-abed/monkey/parser"
+	"github.com/jake-abed/monkey/evaluator"
 	"io"
 )
 
@@ -43,8 +44,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
