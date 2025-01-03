8fe54832ee83a789f166b7c7037269161c659ea5
Jake Abed <jakeabed@proton.me>
2025-01-03 15:47:08 -0800

    Add hash methods to object.go

--- object/object.go
+++ object/object.go
@@ -3,8 +3,10 @@ package object
 import (
 	"bytes"
 	"fmt"
-	"github.com/jake-abed/monkey/ast"
+	"hash/fnv"
 	"strings"
+
+	"github.com/jake-abed/monkey/ast"
 )
 
 type ObjectType string
@@ -118,3 +120,31 @@ type Builtin struct {
 
 func (b *Builtin) Inspect() string  { return "builtin function" }
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
